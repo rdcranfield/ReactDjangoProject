721ffdae7a304123d2d7927bbe65ec18cec191f7
DCwLaptop\dcranfield <dannyaacranfield@gmail.com>
2020-03-04 23:54:36 +0000

    Deleted userupload which wasn't used in the end. Although in future it would be better to have a single app to handle the uploads rather than doing it in the user app/module.

--- jobmanager/jobmanager/settings.py
+++ jobmanager/jobmanager/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'knox',
     'useraccounts',
-    'userupload'
 ]
 
 # AUTH_USER_MODEL = 'useraccounts.CustomUser'
--- jobmanager/userupload/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-# from .models import PDFFile
-# class FileSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = PDFFile
-#         fields = "__all__"
